df6d66aef4fa124208ac040aed98d08b65203a92
atout811 <dmcgfx811@gmail.com>
2022-08-19 21:09:41 +0200

    Two sum

---
+++ two sum.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+class solution {
+    public static int[] sol() {
+        int[] nums = { 3, 3 };
+        int target = 6;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(target - nums[i], i);
+            if (map.containsKey(nums[i]))
+                return new int[] { map.get(nums[i]), i };
+
+            map.put(target - nums[i], i);
+
+        }
+
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        sol();
+    }
+}
