8048f05508ba0edf22ba041ab8ceb1f9562f38cf
atout811 <dmcgfx811@gmail.com>
2022-08-19 19:56:13 +0200

    solve valid anagram

--- Contains Duplicate.java
+++ Contains Duplicate.java
@@ -1,12 +1,13 @@
+import java.util.HashSet;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        for (int num:nums) {
-            if(!set.contains(num)) {
-            set.add(num);
-            }
-            else{
-                 return true;
+        for (int num : nums) {
+            if (!set.contains(num)) {
+                set.add(num);
+            } else {
+                return true;
             }
         }
         return false;
---
+++ Valid Anagram.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+import java.util.*;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        char[] arS = s.toCharArray();
+        char[] arT = t.toCharArray();
+
+        Arrays.sort(arS);
+        Arrays.sort(arT);
+        for (int i = 0; i < arT.length; i++) {
+            if (arS[i] == arT[i])
+                continue;
+            else
+                return false;
+        }
+
+        return true;
+
+    }
+}
