d0d3d2784b15ebf99671c9b62836dca6c1ed23c4
atout811 <dmcgfx811@gmail.com>
2022-08-21 22:28:26 +0200

    solve 206 reverse linked list

---
+++ 206-reverse-linked-list.java
@@ -0,0 +1,32 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = null;
+        ListNode sol = null;
+        ListNode curr = head;
+        while (curr != null) {
+            temp = sol;
+            sol = curr;
+            curr = curr.next;
+            sol.next = temp;
+        }
+        return sol;
+    }
+}
