64ea7b05b7e44ed828489b9cde2401f883a5be05
atout811 <dmcgfx811@gmail.com>
2022-09-01 09:27:35 +0200

    solve 371. Sum of Two Integers

---
+++ 19-remove-nth-node-from-end-of-list.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || head.next == null)
+            return null;
+        ListNode start = new ListNode();
+        start.next = head;
+        ListNode first = start;
+        ListNode second = start;
+
+        while (n > 0) {
+            second = second.next;
+            n--;
+        }
+
+        while (second.next != null) {
+            second = second.next;
+            first = first.next;
+        }
+
+        first.next = first.next.next;
+        return start.next;
+
+    }
+}
