0ae7f4e60833d3281de52ac0e3ee5afc18c1d05d
atout811 <dmcgfx811@gmail.com>
2022-09-02 14:38:42 +0200

    solve 133. Clone Graph

---
+++ 133-clone-graph.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    
+    HashMap<Integer, Node> map = new HashMap<>();
+    
+    public Node cloneGraph(Node node) {
+        if(node == null) return node;
+        if(map.containsKey(node.val)) return map.get(node.val);
+        Node cloned = new Node(node.val, new ArrayList<Node>());
+        map.put(node.val,cloned);
+        for(Node nei : node.neighbors){
+            cloned.neighbors.add(cloneGraph(nei));
+        }
+
+        return cloned;
+    }
+}
